a1e01d3fc52e0b34d8a6c7747054c15227c9bdee
Alerzi88 <alexisht1998@gmail.com>
2018-12-13 01:35:19 -0300

    Cambios en urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,7 +12,6 @@ router.register( r'tecnicos', views.TecnicoViewSet )
 router.register( r'orden', views.OrdenViewSet )
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     url( r'^', include( router.urls ) ),
     url( r'^api-auth/', include( 'rest_framework.urls', namespace='rest_framework' ))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
